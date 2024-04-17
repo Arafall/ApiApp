9d6e5ad734d792adc3776a646aab5a4b46dec80a
Arafall <danielwade101@gmail.com>
2024-04-17 09:54:40 -0500

    Updated color get function
    Color gets data from api properly

--- api/ContentView.swift
+++ api/ContentView.swift
@@ -10,19 +10,19 @@ import SwiftUI
 //https://github.com/cheatsnake/xColors-api?tab=readme-ov-file
 
 struct ContentView: View {
-    struct Color: Codable {
+    struct ColorConvert: Codable {
         var hex: String
         var rgb: String
         var hsl: String
     }
     
     struct Result: Codable {
-        var items: [Color]
+        var item: ColorConvert
     }
     
-    @State var colors:[Color] = []
+    @State var color: ColorConvert
+    var apiColor: Color
     var body: some View {
-        
         
         
         VStack {
@@ -31,19 +31,38 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             
-            
-            
+            Button("Get Color", action: {
+                getColor(colorBase: "red")
+                
+            })
             
         }
         .padding()
     }
     
     func getColor(colorBase: String) {
+
         if let apiUrl = URL(string: "https://x-colors.yurace.pro/api/random/\(colorBase)") {
+            
             var request = URLRequest(url: apiUrl)
             request.httpMethod = "GET"
             URLSession.shared.dataTask(with: request) { data, request, error in
                 if let colorData = data {
+                    if let colorFromAPI = try? JSONDecoder().decode(Result.self, from: colorData) {
+                        color = colorFromAPI.item
+                        print(colorFromAPI)
+                    }
+                    
+                }
+    
+            }.resume()
+            
+            /*
+            var request = URLRequest(url: apiUrl)
+            request.httpMethod = "GET"
+            URLSession.shared.dataTask(with: request) { data, request, error in
+                if let colorData = data {
+                    
                     if let colorsFromAPI = try? JSONDecoder().decode(Result.self, from: colorData) {
                         colors = colorsFromAPI.items
                         print(colors)
@@ -51,6 +70,7 @@ struct ContentView: View {
                 }
     
             }.resume()
+            */
         }
     }
 }
